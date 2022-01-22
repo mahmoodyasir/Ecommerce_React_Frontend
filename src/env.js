c3ef734034cd358957cf3d1c9755bb43bf5718a8
mahmoodyasir <yasirmamun16@gmail.com>
2022-01-22 21:11:07 +0600

    react_commit_8

--- src/env.js
+++ src/env.js
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 
 
 
-export const domain = "http://127.0.0.1:8000";
+export const domain = "https://mobileshopdjango.herokuapp.com";
 export const userToken = window.localStorage.getItem("token")
 export const adminToken = window.localStorage.getItem("admin_token")
 
